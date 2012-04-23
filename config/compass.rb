59126c871e7b77e65fd9d1ead6b6af305164b986
Gabriel Paquin <gpaquin@webagecorp.com>
2012-04-23 14:38:43 +0800

    initial commit

---
+++ config/compass.rb
@@ -0,0 +1,11 @@
+require 'bootstrap-sass'
+
+# Please, be careful when editing these settings, it may break the theme assets API push
+http_path 		= "/"
+css_dir 		= "public/stylesheets"
+sass_dir 		= "public/scss"
+images_dir 		= "public/images"
+javascripts_dir = "public/javascripts"
+output_style 	= :compressed
+line_comments   = false
+project_type    = :stand_alone
